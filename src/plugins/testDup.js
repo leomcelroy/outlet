265151d60bb13f82b2bdd2345bbe5ecd6afd287f
branchwelder <htwiggsmith@gmail.com>
2025-03-27 13:41:46 -0400

    ploogin

--- src/plugins/testDup.js
+++ src/plugins/testDup.js
@@ -3,15 +3,21 @@ import { createRandStr } from "../utils/createRandStr.js";
 export const testDup = {
   type: "testDup",
   init(defaults = {}) {
+    const offset = defaults.offset ?? 10;
     return {
       id: createRandStr(),
       type: "testDup",
       name: "Test Dup",
       controls: [
         {
-          id: "offset",
+          id: "xOffset",
           type: "number",
-          value: 0,
+          value: offset,
+        },
+        {
+          id: "yOffset",
+          type: "number",
+          value: offset,
         },
         // strings
         // numbers
@@ -24,13 +30,30 @@ export const testDup = {
   },
   // children as array of array of geometries
   process(controls, children, attributes) {
-    const { offset } = controls;
+    const { xOffset, yOffset } = controls;
     const newChildren = [];
-    console.log();
-    children.forEach((child) => {
-      newChildren.push(child);
-      newChildren.push(child);
-    });
+
+    children.forEach((child) =>
+      child.forEach((c) => {
+        switch (c.type) {
+          case "point":
+            newChildren.push(c);
+            newChildren.push({ ...c, x: c.x + xOffset, y: c.y + yOffset });
+            break;
+          case "line":
+            newChildren.push(c);
+            newChildren.push({
+              ...c,
+              x1: c.x1 + xOffset,
+              y1: c.y1 + yOffset,
+              x2: c.x2 + xOffset,
+              y2: c.y2 + yOffset,
+            });
+            break;
+        }
+      })
+    );
+
     return newChildren.flat();
   },
 };
