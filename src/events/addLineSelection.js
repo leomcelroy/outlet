aa0ec548bf456bcec6fadf6e72acbaafbd30cf95
leomcelroy <leomcelroy@gmail.com>
2025-03-30 22:04:27 -0400

    Update addLineSelection.js

--- src/events/addLineSelection.js
+++ src/events/addLineSelection.js
@@ -1,8 +1,25 @@
 import { createListener } from "../utils/createListener.js";
+import { patchState } from "../index.js";
 
 export function addLineSelection(el, state) {
   const listener = createListener(el);
 
+  listener("mousedown", "[path]", (e) => {
+    if (e.detail === 2) {
+      // Double click
+      const id = e.target.dataset.id;
+      patchState((state) => {
+        state.editingPath = id;
+        state.selectedGeometry = new Set();
+        state.tool = "SELECT";
+      });
+    } else if (state.tool === "SELECT") {
+      const id = e.target.dataset.id;
+      state.selectedGeometry.add(id);
+    }
+  });
+
+  // Keep existing line selection code
   listener("mousedown", "[line]", (e) => {
     const id = e.target.dataset.id;
     state.selectedGeometry.add(id);
